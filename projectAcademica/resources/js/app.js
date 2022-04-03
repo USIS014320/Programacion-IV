412fc786100a34d3013b0fd94e8207874bf28d51
usis014320 <usis014320@ugb.edu.sv>
2022-04-03 17:47:31 -0600

    Modificaciones menores.

--- projectAcademica/resources/js/app.js
+++ projectAcademica/resources/js/app.js
@@ -23,8 +23,8 @@ window.generarIdUnicoFecha = () => {
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('alumno-component', require('./components/AlumnoComponent.vue').default);
 Vue.component('docente-component', require('./components/DocenteComponent.vue').default);
+Vue.component('alumno-component', require('./components/AlumnoComponent.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
